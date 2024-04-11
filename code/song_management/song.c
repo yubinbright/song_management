2948aaec33b9f18576e63ef83347df16cbfafb92
진률 백 <wii070@naver.com>
2024-04-11 16:40:43 +0900

    맥버전 커밋 오류 테스트/song_list.c생성/메인메뉴 반복동작으로 수정

--- code/song_management/song.c
+++ code/song_management/song.c
@@ -16,19 +16,30 @@ struct Song { //구조체
 
 int main(){
     int mode;
-    printf("menu\n");
-    printf("1. 노래 리스트 관리\n");
-    printf("2. 검색 기능\n");
-    printf("3. 플레이리스트\n");
-    printf("4. 종료\n");
-    printf("메뉴 선택 : ");
-    scanf("%d",&mode);
+    int err = 0;
+    while (1)
+    {
+        if (err == 0) {
+            printf("\n노래 관리 프로그램입니다.\n");
+            printf("원하는 메뉴를 선택하세요.\n\n");
+        }
+        else if (err == 1) {
+            printf("\n해당 메뉴는 존재하지 않습니다.\n");
+            printf("밑의 메뉴 중 선택하세요(예시:1)\n\n");
+            err = 0;
+        }
+        printf("1. 노래 리스트 관리\n");
+        printf("2. 검색 기능\n");
+        printf("3. 플레이리스트\n");
+        printf("4. 종료\n\n");
+        printf("메뉴 선택 : ");
+        scanf(" %d", &mode);
 
-    switch (mode){
+        switch (mode) {
         case 1: //노래 리스트 관리
-            
+
             break;
-        
+
         case 2: //검색 기능
 
             break;
@@ -42,7 +53,10 @@ int main(){
             break;
 
         default: //error
+            err = 1;
             break;
+        }
     }
+    
     return 0;
 }
