827c5ccbdc9d82f70a234bd55c0287d376d0b66b
진률 백 <wii070@naver.com>
2024-04-16 16:14:02 +0900

    재생시간, 앨범 출시 날짜 제외 노래 정보 입력(동명이인 처리 필요)

--- code/song_management/song_list.c
+++ code/song_management/song_list.c
@@ -1,4 +1,5 @@
 #include "header.h"
+#define MAX_SIZE 100000
 
 void song_list_menu() //노래 리스트 주 메뉴
 {
@@ -50,22 +51,232 @@ void song_list_print()//노래 리스트 출력
 {
 	printf("\n노래 리스트를 출력합니다.");
 	printf("\n\n제목 / 가수 / 작곡가 / 작사가 / 장르 / 재생시간 / 앨범명 / 앨범 출시 날짜\n");
-
+	FILE* fp;
+	fp = fopen("song_list.txt", "r");
 }
 
 void song_add()//노래 추가
 {
-	char name[100000];
-	char singer[200];
-	char song_writer[200];
-	char lyric_writer[200];
-	char genre[20];
-	int play_time_hour = 0, play_time_min = 0, play_time_sec = 0;
-	char album_name[200];
-	int album_year = 0, album_month = 0, album_day = 0;
-	printf("?");
+	char name[MAX_SIZE]; //제목
+	char singer[200]; //가수
+	char song_writer[200]; //작곡가
+	char lyric_writer[200]; //작사가
+	char genre[20]; //장르
+	int play_time_hour = 0, play_time_min = 0, play_time_sec = 0; //재생시간들
+	char album_name[200]; //앨범명
+	int album_year = 0, album_month = 0, album_day = 0;//앨범출시날짜
 
+	FILE* fp;
+	fp = fopen("song_list.txt", "a+");
 
+	//제목 입력
+	printf("\n제목을 입력하세요.\n\n");
+	printf("제목 : ");
+	int err=1;
+	getchar();
+	do
+	{
+		gets(name, MAX_SIZE);
+		if (strlen(name) == 0) //입력하지 않았을 때
+		{
+			printf("\n제목 입력이 잘못되었습니다. 정확히 입력해주세요.(예시:좋니)\n\n");
+			printf("제목 : ");
+		}
+		else if (name[0] == ' ' || name[0] == '\t' || name[strlen(name) - 1] == ' ' || name[strlen(name) - 1] == '\t') //앞 뒤에 공백이 있을 때
+		{
+			printf("\n제목 입력이 잘못되었습니다. 정확히 입력해주세요.(예시:좋니)\n\n");
+			printf("제목 : ");
+			name[0] = NULL;
+		}
+		else //맞는 입력일 때
+		{
+			err = 0;
+		}
+	} while (err == 1);
+	//printf("\n%s", name);
+
+	//가수 입력
+	printf("\n가수를 입력하세요.\n\n");
+	printf("가수 : ");
+	err = 1;
+	getchar();
+	do
+	{
+		gets(singer, 200);
+		if (strlen(singer) == 0) //입력하지 않았을 때
+		{
+			printf("\n가수 입력이 잘못되었습니다. 정확히 입력해주세요.(예시:윤종신)\n\n");
+			printf("가수 : ");
+		}
+		else if (singer[0] == ' ' || singer[0] == '\t' || singer[strlen(singer) - 1] == ' ' || singer[strlen(singer) - 1] == '\t') //앞 뒤에 공백이 있을 때
+		{
+			printf("\n가수 입력이 잘못되었습니다. 정확히 입력해주세요.(예시:윤종신)\n\n");
+			printf("가수 : ");
+			singer[0] = NULL;
+		}
+		else //맞는 입력일 때
+		{
+			err = 0;
+		}
+	} while (err == 1);
+	//printf("\n%s", singer);
+
+	//작곡가 입력
+	printf("\n작곡가를 입력하세요.\n\n");
+	printf("작곡가 : ");
+	err = 1;
+	getchar();
+	do
+	{
+		gets(song_writer, 200);
+		if (strlen(song_writer) == 0) //입력하지 않았을 때
+		{
+			printf("\n작곡가 입력이 잘못되었습니다. 정확히 입력해주세요.(예시:윤종신)\n\n");
+			printf("작곡가 : ");
+		}
+		else if (song_writer[0] == ' ' || song_writer[0] == '\t' || song_writer[strlen(song_writer) - 1] == ' ' || song_writer[strlen(song_writer) - 1] == '\t') //앞 뒤에 공백이 있을 때
+		{
+			printf("\n작곡가 입력이 잘못되었습니다. 정확히 입력해주세요.(예시:윤종신)\n\n");
+			printf("작곡가 : ");
+			song_writer[0] = NULL;
+		}
+		else //맞는 입력일 때
+		{
+			err = 0;
+		}
+	} while (err == 1);
+	//printf("\n%s", song_writer);
+
+	//작사가 입력
+	printf("\n작사가를 입력하세요.\n\n");
+	printf("작사가 : ");
+	err = 1;
+	getchar();
+	do
+	{
+		gets(lyric_writer, 200);
+		if (strlen(lyric_writer) == 0) //입력하지 않았을 때
+		{
+			printf("\n작사가 입력이 잘못되었습니다. 정확히 입력해주세요.(예시:윤종신)\n\n");
+			printf("작사가 : ");
+		}
+		else if (lyric_writer[0] == ' ' || lyric_writer[0] == '\t' || lyric_writer[strlen(lyric_writer) - 1] == ' ' || lyric_writer[strlen(lyric_writer) - 1] == '\t') //앞 뒤에 공백이 있을 때
+		{
+			printf("\n작사가 입력이 잘못되었습니다. 정확히 입력해주세요.(예시:윤종신)\n\n");
+			printf("작사가 : ");
+			lyric_writer[0] = NULL;
+		}
+		else //맞는 입력일 때
+		{
+			err = 0;
+		}
+	} while (err == 1);
+	//printf("\n%s", lyric_writer);
+
+	//작사가 입력
+	printf("\n작사가를 입력하세요.\n\n");
+	printf("작사가 : ");
+	err = 1;
+	getchar();
+	do
+	{
+		gets(lyric_writer, 200);
+		if (strlen(lyric_writer) == 0) //입력하지 않았을 때
+		{
+			printf("\n작사가 입력이 잘못되었습니다. 정확히 입력해주세요.(예시:윤종신)\n\n");
+			printf("작사가 : ");
+		}
+		else if (lyric_writer[0] == ' ' || lyric_writer[0] == '\t' || lyric_writer[strlen(lyric_writer) - 1] == ' ' || lyric_writer[strlen(lyric_writer) - 1] == '\t') //앞 뒤에 공백이 있을 때
+		{
+			printf("\n작사가 입력이 잘못되었습니다. 정확히 입력해주세요.(예시:윤종신)\n\n");
+			printf("작사가 : ");
+			lyric_writer[0] = NULL;
+		}
+		else //맞는 입력일 때
+		{
+			err = 0;
+		}
+	} while (err == 1);
+	//printf("\n%s", lyric_writer);
+
+	//장르 입력
+	printf("\n장르를 입력하세요.\n\n");
+	printf("장르 : ");
+	err = 1;
+	getchar();
+	do
+	{
+		gets(genre, 20);
+		if (strcmp(genre,"클래식")==0 || strcmp(genre, "재즈") == 0 || strcmp(genre, "팝") == 0 || strcmp(genre, "발라드") == 0 || strcmp(genre, "힙합") == 0 || strcmp(genre, "트로트") == 0 || strcmp(genre, "디스코") == 0 || strcmp(genre, "댄스") == 0) //맞는 입력일 때
+		{
+			err = 0;
+		}
+		else //규칙과 다를 때
+		{
+			printf("\n장르 입력이 잘못되었습니다. 정확히 입력해주세요.(예시:재즈)\n\n");
+			printf("장르 : ");
+			genre[0] = NULL;
+		}
+	} while (err == 1);
+	//printf("\n%s", genre);
+
+	//재생시간 입력
+	printf("\n재생시간을 입력하세요.\n\n");
+	printf("재생시간 : ");
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요 
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요 
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요 
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요 
+
+	//앨범명 입력
+	printf("\n앨범명을 입력하세요.\n\n");
+	printf("앨범명 : ");
+	err = 1;
+	getchar();
+	do
+	{
+		gets(album_name, 200);
+		if (strlen(album_name) == 0) //입력하지 않았을 때
+		{
+			printf("\n작사가 입력이 잘못되었습니다. 정확히 입력해주세요.(예시:윤종신)\n\n");
+			printf("작사가 : ");
+		}
+		else if (album_name[0] == ' ' || album_name[0] == '\t' || album_name[strlen(album_name) - 1] == ' ' || album_name[strlen(album_name) - 1] == '\t') //앞 뒤에 공백이 있을 때
+		{
+			printf("\n작사가 입력이 잘못되었습니다. 정확히 입력해주세요.(예시:윤종신)\n\n");
+			printf("작사가 : ");
+			album_name[0] = NULL;
+		}
+		else //맞는 입력일 때
+		{
+			err = 0;
+		}
+	} while (err == 1);
+	//printf("\n%s", album_name);
+
+	//앨범 출시 날짜 입력
+	printf("\n앨범 출시 날짜를 입력하세요.\n\n");
+	printf("앨범 출시 날짜 : ");
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요 
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요 
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요 
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요
+	// 수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요수정필요 
 }
 
 void song_dlt(char* filename)//노래 삭제
